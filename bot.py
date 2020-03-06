376b4c1d15e008c4c12a9bec3d055abde77a1fd6
Othil <antoine10120@gmail.com>
2020-03-06 12:12:59 +0100

    VENGA

--- bot.py
+++ bot.py
@@ -1,22 +1,33 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-
 import asyncio
 import time
 import datetime
 import random
-
 from os import walk
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from liste import liste,  liste2
 
 client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+    game = discord.Game(
+        name="{} serveurs | !help les pd".format(len(list(client.guilds)))
+    )
+    await client.change_presence(status=discord.Status.online, activity=game)
+    print("--------------------------------------------")
+    print("(C) Othiil 2019")
+    print("ZbeulBot")
+    print("{} serveurs".format(len(list(client.guilds))))
+    print("--------------------------------------------")
 
 
 @client.command()
 async def rsa(ctx):
-    #Affiche le nombre de jours restant aavant l'éligibilité du khey sam pour son RSA.
+    """ Affiche le nombre de jours restant avant l'éligibilité du khey pour son RSA."""
     tempsActuel = datetime.date.today()
     tempsNotification = datetime.date(2022, 11, 11)
     if tempsActuel > tempsNotification:
@@ -26,45 +37,21 @@ async def rsa(ctx):
 
 
 @client.command()
-async def zebi(ctx):
-    #Poste une image aléatoire depuis un fichier spécifié.
-    img = [
-        "0.jpg, 1.jpg",
-        "2.jpg",
-        "3.jpg",
-        "4.jpg",
-        "5.jpg",
-        "6.jpg",
-        "7.jpg",
-        "8.jpg",
-        "9.jpg",
-        "10.jpg",
-        "11.jpg",
-        "12.jpg",
-        "13.jpg",
-        "14.jpg"
-        "16.jpg"
-        """,'17.jpg','18.jpg','19.jpg', '20.jpg','21.jpg','22.jpg','23.jpg','24.jpg', '25.jpg'""",
-    ]
-    await ctx.send(file=discord.File(random.choice(img)))
-
-
-@client.command()
 async def dragon(ctx):
-    #Commande pas très utile mais qui fait référence à fianso.
-    await ctx.send("ROULE UN DRAGON") 
+    """fianso est un khey deter , et toi ? :) """
+    await ctx.send("ROULE UN DRAGON")
     await asyncio.sleep(1.5)
     await ctx.send("FUME LE DRAGON")
     await asyncio.sleep(1.5)
     await ctx.send("CHARGE LE DRAGON ")
     await asyncio.sleep(1.5)
     await ctx.send("ET JE VISE LES TES-TÊ")
-    await ctx.send(file=discord.File("fianso.png")) 
+    await ctx.send(file=discord.File("fianso.png"))
 
 
 @client.command()
 async def chomeur(ctx):
-    #Temps de jeu du khey sam , meilleur hpal fr 
+    """Temps de jeu du khey sam , meilleur hpal fr"""
     await ctx.send(
         "TEMPS DE JEU AU 1/1/18 :\n\nHpal > 1657h \nMonk > 235h\nDruid > 35h\nHunt > 464h\nRogue > 66h\n\nTOTAL > 2457h"
     )
@@ -76,25 +63,9 @@ async def chomeur(ctx):
     )
     await ctx.send(file=discord.File("rsa.png"))
 
-
-@client.command(pass_context=True)
-async def ping(ctx):
-    #Commande ping , j'ai vraiment besoin de la décrire ? 
-    channel = ctx.message.channel
-    t1 = time.perf_counter()
-    await ctx.send(channel)
-    t2 = time.perf_counter()
-    embed = discord.Embed(
-        title="Pong!",
-        description="Ok {}ms.".format(round((t2 - t1) * 1000)),
-        color=0xFFFFFF,
-    )
-    await ctx.send(embed=embed)
-
-
 @client.command()
 async def risibank(ctx):
-    # Cette commande poste juste une image aléatoire de risibank
+    """ça poste juste une image aléatoire de risibank"""
     url = "https://risibank.fr/stickers/hasard"
     headers = {"User-Agent": "Mozilla/5.0"}
 
@@ -105,14 +76,39 @@ async def risibank(ctx):
 
 
 @client.command()
-async def img(ctx):
-    #Même chose que plus haut , sauf que c'est pas le même dossier.
-    f = []
-    for (dirpath, dirnames, filenames) in walk("path"):
+async def sg(ctx):
+    """Post une photo aléatoire.(NSFW)"""
+   # path = "/run/media/antoine/127E02767E0252C1/RisiBot/satin/"
+    """f = []
+    for (dirpath, dirnames, filenames) in walk(path):
         f.extend(filenames)
         break
-    sendfile = random.choice(f)
-    await ctx.send(file = discord.File('path'+ sendfile))
+    sgfile = random.choice(f)
+    await ctx.send(file=discord.File(path + sgfile))"""
 
+    rdming = random.choice(liste)
+    if ctx.channel.is_nsfw():
+        await ctx.send(rdming)
+    else:
+        await ctx.send("Pas dans le bon chan gros fdp")
 
-client.run("token")
+@client.command()
+async def bd(ctx):
+    rdming = random.choice(liste2)
+    if ctx.channel.is_nsfw():
+        await ctx.send(rdming)
+    else:
+        await ctx.send("Pas dans le bon chan gros fdp")
+
+@client.command()
+async def wiki(ctx):
+    """C'est juste pour avoir un article random sur wikipedia, histoire que tu sois moins con fdp"""
+    await ctx.send('Clique et instruit toi, sombre merde.')
+    await ctx.send('https://fr.wikipedia.org/wiki/Spécial:Page_au_hasard')
+
+@client.command()
+async def jvc(ctx):
+    """L'élite se doit de connaître ses classiques"""
+    await ctx.send('https://wiki.jvflux.com/Sp%C3%A9cial:Page_au_hasard')
+
+client.run("NDQyMzAwNjI3NTk5NDkxMDcz.XLnuzA.CUoOzgr19GLUWsCE9ydmTwnfCsE", bot=True, reconnect=True)
